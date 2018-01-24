a1ac841fa4cfa706e7543dc7e10ea43b2112fdab
Daemonsword <halim.mezine7@gmail.com>
2018-01-24 16:07:48 +0100

    fdp

--- afficherTableau.php
+++ afficherTableau.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
 	<link rel="stylesheet" media="screen" type="text/css" title="index" href="css/menu.css" />
 	<title></title>
 </head>
 <body>
 	<table border="1" style="text-align: center">
-=======
-	<title></title>
-</head>
-<body>
-	<table border="1">
->>>>>>> parent of 9ad3e21... finish
 		<?php
 		$bdd = new PDO('mysql:host=localhost;dbname=tableaumktn;charset=utf8', 'root', ''); 
 		$k=0;
@@ -20,19 +13,11 @@
 		for ($t=1; $t<=4; $t++){
 			?>
 
-<<<<<<< HEAD
 		<tr><td class ="tete" colspan="13">Tete <?php echo $t?></caption>
 			<tr class="cassette">
 				
 				<td></td>
 				<td colspan="2">Cassette 1</td><!--td horizontale et tr verticale-->
-=======
-
-		?>
-		<tr><td colspan="12">Tete <?php echo $t?></caption>
-			<tr>
-				<td colspan="2">Cassette 1</td>
->>>>>>> parent of 9ad3e21... finish
 				<td colspan="2">Cassette 2</td>
 				<td colspan="2">Cassette 3</td>
 				<td colspan="2">Cassette 4</td>
@@ -40,7 +25,6 @@
 				<td colspan="2">Cassette 6</td>
 				
 			</tr>
-<<<<<<< HEAD
 
 				<tr>
 					<td></td>
@@ -56,21 +40,6 @@
 					<td class="lettre"> J </td>
 					<td class="lettre"> K </td>
 					<td class="lettre"> L </td>
-=======
-				<tr>
-					<td> A </td>
-					<td> B </td>
-					<td> C </td>
-					<td> D </td>
-					<td> E </td>
-					<td> F </td>
-					<td> G </td>
-					<td> H </td>
-					<td> I </td>
-					<td> J </td>
-					<td> K </td>
-					<td> L </td>
->>>>>>> parent of 9ad3e21... finish
 					
 				</tr>
 				
@@ -107,7 +76,7 @@
     $pm->execute();
     $donneesPM = $pm->fetchAll(PDO::FETCH_ASSOC);
 
-    $idPM = $_POST["idPM"];
+    $idPM = ["idPM"];
 
     $req =  $bdd -> prepare('SELECT * FROM fibre, pm WHERE fibre.idPM = pm.idPM AND pm.idPM = "$idPM"');
     $req -> execute();
