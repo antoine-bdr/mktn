985ecf36b44a5f39f7530c46e25f64c28087e978
antoine-bdr <antoinebandarra@gmail.com>
2018-01-24 09:57:02 +0100

    Remplir PM

--- remplirTableau2.php
+++ remplirTableau2.php
@@ -1,4 +1,3 @@
-
 <?php 
 $bdd = new PDO('mysql:host=localhost;dbname=tableaumktn;charset=utf8', 'root', ''); 
 session_start();
@@ -6,10 +5,10 @@ session_start();
 ini_set( 'display_errors', 'on' );
 error_reporting( E_ALL );
 
+
 if (isset($_POST['bouton'])) {
 
         //Insertion des données dans la bdd
-        $cassette = htmlspecialchars(trim($_POST['cassette']));
         $epissure = htmlspecialchars(trim($_POST['epissure']));
         $PA = htmlspecialchars(trim($_POST['PA']));
         $TR = htmlspecialchars(trim($_POST['TR']));
@@ -38,16 +37,84 @@ if (isset($_POST['bouton'])) {
 <?php include ("menu.php") ?>
   <form action="#" method="POST">
     <fieldset>
-          <legend>  Choisir la tête du PM</legend>
+          <legend> Configuration du PM <?php echo $_SESSION['idPM']; ?> </legend>
 
-          <label for="tete">Tete: </label>
-          <input  name="tete"/><br><br> 
+          <label for="tete">Tête: </label>
+          <SELECT name="tete" size="1">
+            <OPTION>1
+            <OPTION>2
+            <OPTION>3
+            <OPTION>4
+          </SELECT><br><br>
 
           <label for="cassette">Cassette: </label>
-          <input type="text" name="cassette"/><br><br>
+          <SELECT name="cassette" size="1">
+            <OPTION>Cassette A
+            <OPTION>Cassette B
+            <OPTION>Cassette C
+            <OPTION>Cassette D
+            <OPTION>Cassette E
+            <OPTION>Cassette F
+            <OPTION>Cassette G
+            <OPTION>Cassette H
+            <OPTION>Cassette I
+            <OPTION>Cassette J
+            <OPTION>Cassette K
+            <OPTION>Cassette L
+          </SELECT><br><br>
+
+          <button type="submit" name="confirmer" ><b>Confirmer</b></button><br><br>
+          <?php
+          if (isset($_POST['confirmer'])) {
+              $tete = htmlspecialchars(trim($_POST['tete']));
+              $cassette = htmlspecialchars(trim($_POST['cassette']));
+              
+              ?>
 
            <label for="epissure">Epissure: </label>
-          <input type="text" name="epissure"/><br><br>
+            <SELECT name="epissure" size="1">
+              <?php if ($cassette == "Cassette A") { ?>
+              <OPTION>1-6
+              <OPTION>7-12
+              <?php }elseif ($cassette == "Cassette B") { ?>
+              <OPTION>13-18
+              <OPTION>19-24
+              <?php }elseif ($cassette == "Cassette C") { ?>
+              <OPTION>25-30
+              <OPTION>31-36
+              <?php }elseif ($cassette == "Cassette D") { ?>
+              <OPTION>37-42
+              <OPTION>43-48
+              <?php }elseif ($cassette == "Cassette E") { ?>
+              <OPTION>49-54
+              <OPTION>55-60
+              <?php }elseif ($cassette == "Cassette F") { ?>
+              <OPTION>61-66
+              <OPTION>67-72
+              <?php }elseif ($cassette == "Cassette G") { ?>
+              <OPTION>73-78
+              <OPTION>79-84
+              <?php }elseif ($cassette == "Cassette H") { ?>
+              <OPTION>85-90
+              <OPTION>91-96
+              <?php }elseif ($cassette == "Cassette I") { ?>
+              <OPTION>97-102
+              <OPTION>103-108
+              <?php }elseif ($cassette == "Cassette J") { ?>
+              <OPTION>109-114
+              <OPTION>115-120
+              <?php }elseif ($cassette == "Cassette K") { ?>
+              <OPTION>121-126
+              <OPTION>127-132
+              <?php }elseif ($cassette == "Cassette L") { ?>
+              <OPTION>133-138
+              <OPTION>139-144
+              <?php } ?>
+              
+              
+              
+
+          </SELECT><br><br>
 
            <label for="PA">PA: </label>
           <input type="text" name="PA"/><br><br>
@@ -60,6 +127,8 @@ if (isset($_POST['bouton'])) {
 
           <button type="submit" name="bouton" ><b>Créer</b></button>
 
+          <?php } ?>
+
 
       </fieldset>
   </form>
@@ -71,6 +140,7 @@ if (isset($_POST['bouton'])) {
   if(isset($message)){
     echo $message;
   }
+
   ?>
 </body>
 </html>
